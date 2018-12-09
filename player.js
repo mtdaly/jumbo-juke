48158ff1cc209d91a275bc962cf4db983359a31c
Matt Daly <mattdaly1235@gmail.com>
2018-12-09 15:48:59 -0500

    database management updates

--- player.js
+++ player.js
@@ -51,11 +51,11 @@ function getCredentials() {
 app.get("/", function (request, response) {
     console.log("entered get callback");
     // getCredentials();
-    if (songs.count() === 0) {
+    // if (songs.count() === 0) {
         console.log("Adding songs to empty collection");
         addSongsFromPlaylist(top50);
         addSongsFromPlaylist(spotifySingles);
-    }
+    // }
     response.send("this is a test");
 });
 
