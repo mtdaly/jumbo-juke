2920172cdc196b688b4911e5cd2cbaf6f23e72d0
Matt Daly <mattdaly1235@gmail.com>
2018-12-09 16:32:57 -0500

    testing

--- player.js
+++ player.js
@@ -47,20 +47,20 @@ function getCredentials() {
 
 function addSongs() {
     songs.remove({});
-    spotifyApi.clientCredentialsGrant().then(
-        function (data) {
-            console.log('The access token expires in ' + data.body['expires_in']);
-            console.log('The access token is ' + data.body['access_token']);
-
-            // Save the access token so that it's used in future calls
-            spotifyApi.setAccessToken(data.body['access_token']);
-            addSongsFromPlaylist(top50);
-            addSongsFromPlaylist(spotifySingles);
-        },
-        function (err) {
-            console.log('Something went wrong when retrieving an access token', err);
-        }
-    );
+    // spotifyApi.clientCredentialsGrant().then(
+    //     function (data) {
+    //         console.log('The access token expires in ' + data.body['expires_in']);
+    //         console.log('The access token is ' + data.body['access_token']);
+    //
+    //         // Save the access token so that it's used in future calls
+    //         spotifyApi.setAccessToken(data.body['access_token']);
+    //         addSongsFromPlaylist(top50);
+    //         addSongsFromPlaylist(spotifySingles);
+    //     },
+    //     function (err) {
+    //         console.log('Something went wrong when retrieving an access token', err);
+    //     }
+    // );
 }
 
 //// REQUEST HANDLING ////
