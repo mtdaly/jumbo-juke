3415379484d6888dd152a8bd39c7d000e41934d6
Matt Daly <mattdaly1235@gmail.com>
2018-12-09 15:47:46 -0500

    database management updates

--- player.js
+++ player.js
@@ -50,7 +50,7 @@ function getCredentials() {
 
 app.get("/", function (request, response) {
     console.log("entered get callback");
-    getCredentials();
+    // getCredentials();
     if (songs.count() === 0) {
         console.log("Adding songs to empty collection");
         addSongsFromPlaylist(top50);
