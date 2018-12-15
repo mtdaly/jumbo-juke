da5f238dfd541634f540757503a3f86683a3f3a1
Matt Daly <mattdaly1235@gmail.com>
2018-12-15 17:40:27 -0500

    more refactoring

--- public/request.js
+++ public/request.js
@@ -1,52 +1,31 @@
 $(document).ready(function () {
 
     $(document).on('click', "#getSong", function () {
-
-        // Get parameters from sliders
-        // var pD = document.getElementById("dance").value / 100;
-        // var pA = document.getElementById("acoustic").value / 100;
-        // var pE = document.getElementById("energy").value / 100;
-        // var pT = ((document.getElementById("tempo").value) * 1.5) + 50;
-
-        // Use parameters in AJAX request to get a song
-        var params = {
-            dance: document.getElementById("dance").value / 100,
-            acoustic: document.getElementById("acoustic").value / 100,
-            energy: document.getElementById("energy").value / 100,
-            tempo: (document.getElementById("tempo").value * 1.5) + 50
-        };
-        getSong(params);
-
+        getSong();
     });
 
     $(document).on('click', "#rand", function () {
 
+        // Needs enable check
         // Set sliders/params to random values
         document.getElementById("dance").value = Math.floor(Math.random() * 100);
         document.getElementById("acoustic").value = Math.floor(Math.random() * 100);
         document.getElementById("energy").value = Math.floor(Math.random() * 100);
         document.getElementById("tempo").value = getRandomIntInclusive(0, 100)
 
-        var params = {
-            dance: document.getElementById("dance").value / 100,
-            acoustic: document.getElementById("acoustic").value / 100,
-            energy: document.getElementById("energy").value / 100,
-            tempo: (document.getElementById("tempo").value * 1.5) + 50
-        };
-        getSong(params);
+        getSong();
 
     });
 });
 
-function getSong(params) {
+function getSong() {
     $.ajax({
         type: "POST",
         url: "https://jumbo-juke.herokuapp.com/getPreview",
-        data: JSON.stringify(params),
+        data: JSON.stringify(getParams()),
         contentType: "application/JSON; charset=utf-8",
         success: function (data) {
-            // next line could be useful for building reccomendation history
-            // var oldUrl = $("#music_player").attr("src"); // Get current url
+            console.log(data);
             var newUrl = "https://open.spotify.com/embed/track/" + data.substring(14);
             $("#music_player").attr("src", newUrl);
         },
@@ -56,6 +35,15 @@ function getSong(params) {
     });
 }
 
+// After upgrading sliders, add enable check here...
+function getParams() {
+    return {
+        dance: document.getElementById("dance").value / 100,
+        acoustic: document.getElementById("acoustic").value / 100,
+        energy: document.getElementById("energy").value / 100,
+        tempo: (document.getElementById("tempo").value * 1.5) + 50
+    };
+}
 
 // Code from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomIntInclusive(min, max) {
@@ -63,3 +51,11 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+
+/// Notes...
+
+// line could be useful for building reccomendation history
+// var oldUrl = $("#music_player").attr("src"); // Get current url
