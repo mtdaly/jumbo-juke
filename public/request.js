6d0d140ffe1de9236421531683495ab675043fd8
Matt Daly <mattdaly1235@gmail.com>
2018-12-15 17:26:35 -0500

    random now gets a song too, refactoring

--- public/request.js
+++ public/request.js
@@ -1,51 +1,59 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	$(document).on('click', "#getSong", function(){
+    $(document).on('click', "#getSong", function () {
 
-		// Get parameters from sliders
-		var pD = document.getElementById("dance").value / 100;
-		var pA = document.getElementById("acoustic").value / 100;
-		var pE = document.getElementById("energy").value / 100;
-		var pT = ((document.getElementById("tempo").value) * 1.5) + 50;
+        // Get parameters from sliders
+        // var pD = document.getElementById("dance").value / 100;
+        // var pA = document.getElementById("acoustic").value / 100;
+        // var pE = document.getElementById("energy").value / 100;
+        // var pT = ((document.getElementById("tempo").value) * 1.5) + 50;
 
-		// Use parameters in AJAX request to get a song
-		var params = {
-					dance: pD,
-					acoustic: pA,
-					energy: pE,
-					tempo: pT
-				};
-		getSong(params);
+        // Use parameters in AJAX request to get a song
+        var params = {
+            dance: document.getElementById("dance").value / 100,
+            acoustic: document.getElementById("acoustic").value / 100,
+            energy: document.getElementById("energy").value / 100,
+            tempo: (document.getElementById("tempo").value * 1.5) + 50
+        };
+        getSong(params);
 
-	});
+    });
 
-    $(document).on('click', "#rand", function() {
+    $(document).on('click', "#rand", function () {
 
         // Set sliders/params to random values
-        document.getElementById("dance").value = Math.floor(Math.random()*100);
-        document.getElementById("acoustic").value = Math.floor(Math.random()*100);
-        document.getElementById("energy").value = Math.floor(Math.random()*100);
+        document.getElementById("dance").value = Math.floor(Math.random() * 100);
+        document.getElementById("acoustic").value = Math.floor(Math.random() * 100);
+        document.getElementById("energy").value = Math.floor(Math.random() * 100);
         document.getElementById("tempo").value = getRandomIntInclusive(0, 100)
+
+        var params = {
+            dance: document.getElementById("dance").value / 100,
+            acoustic: document.getElementById("acoustic").value / 100,
+            energy: document.getElementById("energy").value / 100,
+            tempo: (document.getElementById("tempo").value * 1.5) + 50
+        };
+        getSong(params);
 
     });
 });
 
 function getSong(params) {
-	$.ajax({
-		type: "POST",
-		url: "https://jumbo-juke.herokuapp.com/getPreview",
-		data: JSON.stringify(params),
-		contentType: "application/JSON; charset=utf-8",
-		success: function(data) {
-		    // next line could be useful for building reccomendation history
-			// var oldUrl = $("#music_player").attr("src"); // Get current url
-        	var newUrl = "https://open.spotify.com/embed/track/" + data.substring(14);
-        	$("#music_player").attr("src", newUrl);
-		},
-		error: function(err) {
-			console.log(err);
-		}
-	});
+    $.ajax({
+        type: "POST",
+        url: "https://jumbo-juke.herokuapp.com/getPreview",
+        data: JSON.stringify(params),
+        contentType: "application/JSON; charset=utf-8",
+        success: function (data) {
+            // next line could be useful for building reccomendation history
+            // var oldUrl = $("#music_player").attr("src"); // Get current url
+            var newUrl = "https://open.spotify.com/embed/track/" + data.substring(14);
+            $("#music_player").attr("src", newUrl);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
 }
 
 
